7c1dc33f53729184d26fbdd70522c2f9766b2319
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-08-08 00:23:33 +0100

    Adds quiz data

---
+++ client/src/components/quizData.js
@@ -0,0 +1,109 @@
+[
+    {
+        id: 1,
+        question: 'When is Harry Potters Birthday?',
+        options: ['21 October', '11 June', '31 July', '1 August'], 
+        answer: "31 July"
+    },
+     {
+      id: 2,
+      question: 'What do Hermione Grangers parents do for a living?',
+      options: ['Doctors', 'Dentists' ,'Lecturers', 'Lawyers'], 
+      answer: "Dentists"
+  
+     }, 
+     {
+     id: 3,
+        question: 'Who is the headteacher of Beauxbatons Academy of Magic?',
+        options: ['Igor Karkaroff', 'Armando Dippet' ,'Agilbert Fontaine', 'Madame Maxine'], 
+        answer: "Madame Maxine"
+     },
+     {
+  
+      id: 4,
+        question: 'What is the name of Voldemorts snake?',
+        options: ['Ningini', 'Nungini', 'Nengani', 'Nagina'], 
+        answer: "Nagini"
+  
+     }, {
+  
+      id: 5,
+      question: 'Where did Vernon Dursley work?',
+      options: ['Groundings', 'Grunnings' ,'Grunlings', 'Bank of England'], 
+      answer: "Grunnings"
+     }, {
+  
+      id: 6,
+        question: 'What is Voldemorts middle name?',
+        options: ['Michael', 'Marvin' , 'Maurice', 'Marvolo'], 
+        answer: 'Marvolo'
+     }, {
+        id: 7,
+        question: 'What type of dragon was Hagrids pet?',
+        options: ['Romanian Longhorn', 'Hungarian Horntail' , 'Peruvian Vipertooth', 'Norwegian Ridgeback'], 
+        answer: 'Norwegian Ridgeback'
+
+     }, {
+        id: 8,
+        question: 'What is Madame Hoochs first name?',
+        options: ['Rolanda', 'Rowena' , 'Jolande', 'Rowina'], 
+        answer: 'Rolanda'
+
+     }, {
+
+        id: 9,
+        question: 'What disparaging name is given to people who were born into a wizarding family but who do not have any powers?',
+        options: ['Squib', 'Squab' , 'Squeb', 'Squob'], 
+        answer: 'Squib'
+     }, {
+
+        id: 10,
+        question: 'What school was Harry supposed to attend instead of Hogwarts?',
+        options: ['Stanwell', 'Stonewall' , 'Ridgewell', 'Stonely'], 
+        answer: 'Stonewall'
+     }, {
+        id: 11,
+        question: 'Where was Harry Potter born',
+        options: ['Godrics Hollow', 'Geldrics Hallow' , 'Golders Green', 'Galway Grove'], 
+        answer: 'Godrics Hollow'
+
+     }, {
+
+        id: 12,
+        question: 'Who was the inventor behind the Sorcerers Stone?',
+        options: ['Ludo Bagman', 'Celestina Warbeck' , 'Gellert Grindelwald', 'Nicholas Flamel'], 
+        answer: 'Nicholas Flamel'
+     }, {
+        id: 13,
+        question: 'What did Nevilles grandmother give in when he was in first year at Hogwarts?',
+        options: ['Probity Probe', 'Sneakoscope' , 'Remembrall', 'Revealer'], 
+        answer: 'Remembrall'
+
+     }, {
+
+        id: 14,
+        question: 'What is the name of Fred and George Wealeys joke shop?',
+        options: ['Zonkos Joke Shop', 'Weasley Joke Emporium' , 'Leaping Lizards', 'Wealeys Wizard Wheezes'], 
+        answer: 'Weasleys Wizard Wheezes'
+     }, {
+        id: 15,
+        question: 'Who is not a member of the Order of the Phoneix?',
+        options: ['Remus Lupin', 'Mad-eye Moody' , 'Cornelius Fudge', 'Severus Snapes'], 
+        answer: 'Cornelius Fudge'
+
+     }, {
+        id: 16,
+        question: 'What model was Harry Potters first broomstick?',
+        options: ['Firebolt', 'Cleansweep One' , 'Nimbus 2000', 'Comet Two Ninety'], 
+        answer: 'Nimbus 2000'
+
+     }, {
+        id: 17,
+        question: 'What does the spell Obliviate do?',
+        options: ['makes the subject dance', 'deweaponize' , 'unlock doors', 'erases memories'], 
+        answer: 'erases memories'
+
+     }
+        
+       
+  ]
