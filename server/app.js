900b5c2c557c718f626198e18b88f01870586861
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-08-07 19:00:16 +0100

    Integrates socket.io and initializes frontend using CRA. Adds new index.js file

--- server/app.js
+++ server/app.js
@@ -8,10 +8,17 @@ const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 
+const socketio = require('socket.io');
 
+const io = socketio(server);
 
 
 app.use(router);
+
+
+io.on('connection', (socket) => {
+    console.log('Connecting....')
+});
 
 server.listen(PORT, () => {
     console.log(`Listening for requests on port ${PORT}`)
