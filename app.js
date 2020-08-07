8f0fa763e5a85950b8488172f043ecbd7481aa2f
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-08-07 18:47:23 +0100

    Adds routing file and requires into app.js

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express');
 
+const router = require('./router.js');
 
 const http = require('http');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(app);
+
+
+
+
+app.use(router);
 
 server.listen(PORT, () => {
     console.log(`Listening for requests on port ${PORT}`)
