efa6ea519d643df40091e5aee5ff704c84aadb45
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-08-10 20:59:20 +0100

    Adds alert messages which I will later replace with flash messages. If the user doesn't enter text in all three message they see a message and a different alert when they enter text into all three fields

--- client/src/components/User/User.js
+++ client/src/components/User/User.js
@@ -2,6 +2,7 @@ import React, { Fragment, useState} from 'react';
 import { Link } from 'react-router-dom';
 import Header from '../UI/Header/Header';
 
+
 import classes from './User.module.css';
 
 const User = () => {
@@ -11,14 +12,18 @@ const User = () => {
     const [name, setName] = useState('');
     const [chatroom, setChatroom] = useState('');
 
+    let message = "Please enter a name, username and room"
+
     return (
         <>
           <Header />
           <h1 className={classes.PotterHead}>Potterhead</h1>
+          
           <input type="text" placeholder="Please enter a username" className="inputBox" onChange={ (event) => setUsername(event.target.value) }></input>        
           <input type="text" placeholder="Please enter your name" className="inputBox" onChange={ (event) => setName(event.target.value) }></input>
           <input type="text" placeholder="Please enter a room" className="inputBox" onChange={ (event) => setChatroom(event.target.value) }></input>
-          <Link to={`/chatroom?name=${name}&username=${username}&chatroom=${chatroom}`}>
+          <Link to={`/chatroom?name=${name}&username=${username}&chatroom=${chatroom}`} onClick={event => (!username || !name || !chatroom) ? alert('Please enter a name username and room') && event.preventDefault() : alert('Welcome to the chatroom') } >
+           
             <button className="button" >Proceed to Chatrooms</button>
           </Link>
         
