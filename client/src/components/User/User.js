5be8c78b671f6ad62b32b12eb92106fa91a87f05
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-08-10 19:39:52 +0100

    Adds input fields with button and link to the ChatRoom component

--- client/src/components/User/User.js
+++ client/src/components/User/User.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useState} from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../UI/Header/Header';
 
 import classes from './User.module.css';
@@ -7,12 +8,20 @@ const User = () => {
 
 
     const [username, setUsername] = useState('');
+    const [name, setName] = useState('');
     const [chatroom, setChatroom] = useState('');
 
     return (
         <>
           <Header />
           <h1 className={classes.PotterHead}>Potterhead</h1>
+          <input type="text" placeholder="Please enter a username" className="inputBox" onChange={ (event) => setUsername(event.target.value) }></input>        
+          <input type="text" placeholder="Please enter your name" className="inputBox" onChange={ (event) => setName(event.target.value) }></input>
+          <input type="text" placeholder="Please enter a room" className="inputBox" onChange={ (event) => setName(event.target.value) }></input>
+          <Link to={"/chatroom"}>
+            <button className="button" >Proceed to Chatrooms</button>
+          </Link>
+        
         </>
     )
 
