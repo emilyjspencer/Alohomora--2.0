ad4a0f1eb14ea1e481d2352b4797c28f1dffe2e3
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-10-13 20:53:41 +0100

    Pulling in data from the characters endpoint of the Harry Potter API and displaying it on the page

--- client/src/components/Characters/Characters.js
+++ client/src/components/Characters/Characters.js
@@ -1,29 +1,55 @@
-import React, { fragment, useState, useEffect } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import axios from 'axios';
 
 import Cards from '../Cards/Cards';
 import Header from '../shared/UI/Header/Header';
 
-import characters from '../../images/Muggles.png';
+import muggles from '../../images/muggles.png';
 
 import './Characters.css';
 
 
 const Characters = () => {
 
-   
+   const [characters, setCharacters] = useState([]);
+    
+        useEffect(() => {
+            
+            const key='$2a$10$0TXo.XhZgsclRMsBTLHsx.5fYykcXOwOWF.12o20jBumw.10YEat2'
+            //const key = process.env.REACT_APP_API_KEY;
+            console.log(key)
+            
+            axios.get(`https://www.potterapi.com/v1/characters?key=${key}`)
+              .then(result => {
+                 console.log(result)
+                  const characters = result.data;
+                 console.log(characters)
+                  console.log(characters[0]);
+                    setCharacters(characters)
+              });
+            }, []);
 
-
+      console.log(characters)
 
     return (
         <div className="characters">
           <Header />
           <h1>Characters</h1>
-          <img className="characters" src={characters} alt="characters" />
-          <Cards />
-            <ul>
-                
+          <img className="muggles" src={muggles} alt="characters" />
+          <br />
+          <ul>
+                {characters.map(character => (
+                    <>
+                    <li key={character.id}>
+                        <strong><p>Name of character:  {character.name}  </p> </strong> 
+                        <strong><p>Character's role: {character.role} </p></strong>
+                        <strong><p>Character's house: {character.house} </p></strong>
+                    </li>
+                    </>
+                ))}
             </ul>
+               
+        
         </div>
         
     )
